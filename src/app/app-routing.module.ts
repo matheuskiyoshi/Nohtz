089fb11461a85ee045c02881c85e2707f887d093
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-27 16:44:01 -0300

    feat(routes): Update app-routing with required routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/components/login/login.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'notes', loadChildren: () => import('./modules/notes/notes.module').then(m => m.NotesModule) },
+  { path: 'register', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
