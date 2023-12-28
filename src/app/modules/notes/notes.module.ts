1f7842cf1f22f9c87be2e96afa3bb05132efead6
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-28 12:47:02 -0300

    feat(notes): Add 'notes-create' and 'notes-list' to notes module

--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotesRoutingModule } from './notes-routing.module';
+import { NotesCreateComponent } from './components/notes-create/notes-create.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotesCreateComponent,
+    NotesListComponent
+  ],
   imports: [
     CommonModule,
     NotesRoutingModule
