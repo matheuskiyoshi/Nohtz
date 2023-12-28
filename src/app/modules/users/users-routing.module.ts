bfbef55c7537be73a848f4179e0967b43d7d9683
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-27 23:32:31 -0300

    feat(users): Add 'user-register' as default route for 'register'

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserRegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
