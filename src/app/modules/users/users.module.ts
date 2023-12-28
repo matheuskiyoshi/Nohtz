c11db993648dc97800cb4f4dc4c5b62141807490
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-27 21:38:03 -0300

    feat(users): Add 'ReactiveFormsModule' in imports

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { UserRegisterComponent } from './components/user-register/user-register.
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UsersModule { }
