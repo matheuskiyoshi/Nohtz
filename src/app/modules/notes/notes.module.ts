6267d6387074acc07f857687d36f39e3362dbdd1
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-28 15:10:02 -0300

    feat(notes): Add providers to notes module and add 'ReactiveFormsModule' as imports

--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { NotesRoutingModule } from './notes-routing.module';
 import { NotesCreateComponent } from './components/notes-create/notes-create.component';
 import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from 'src/app/core/interceptors/request.interceptor';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +16,11 @@ import { NotesListComponent } from './components/notes-list/notes-list.component
   ],
   imports: [
     CommonModule,
-    NotesRoutingModule
+    NotesRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true },
   ]
 })
 export class NotesModule { }
