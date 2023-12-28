fae35044505773370dfaff7f81f2c8588a3fa1ea
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-27 21:36:25 -0300

    feat(user): Add 'user-register' component to users module

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserRegisterComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
