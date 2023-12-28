1d66f7e8a027a480a71ad13171129408dfe44bc5
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-27 21:41:12 -0300

    feat(users): Add methods used in the component

--- src/app/modules/users/components/user-register/user-register.component.ts
+++ src/app/modules/users/components/user-register/user-register.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { first } from 'rxjs';
+import { UserService } from './../../services/user.service';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.sass']
 })
-export class UserRegisterComponent {
+export class UserRegisterComponent implements OnInit{
+  form!: FormGroup;
+  submitted = false;
+
+  constructor(private userService: UserService, private router: Router ){}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  private buildForm(): void{
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  onRegister(){
+    this.submitted = true;
+    if (this.form.valid) {
+      this.userService
+        .create(this.form.getRawValue())
+        .pipe(first())
+        .subscribe({
+          next: (createdUser: any) => {
+            this.router.navigate(['/login']);
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+    }
+  }
 
 }
