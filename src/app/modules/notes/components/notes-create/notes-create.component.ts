8f00af6071982ddd145f8e0954281dadeaa500a6
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-28 15:18:15 -0300

    feat(notes): Add methods and functionalities to the 'notes-create' component

--- src/app/modules/notes/components/notes-create/notes-create.component.ts
+++ src/app/modules/notes/components/notes-create/notes-create.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { LoginService } from './../../../../core/auth/services/login.service';
+import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../../services/notes.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-notes-create',
   templateUrl: './notes-create.component.html',
   styleUrls: ['./notes-create.component.sass']
 })
-export class NotesCreateComponent {
+export class NotesCreateComponent implements OnInit {
+  form!: FormGroup;
+  submitted = false;
+  private noteId?: string;
+  userId!: string;
 
+  constructor(
+    private noteService: NotesService,
+    private location: Location,
+    private route: ActivatedRoute,
+    private loginService: LoginService,
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = this.loginService.getUserId();
+    this.buildForm();
+    this.getIdFromUrl();
+  }
+
+  private buildForm(): void{
+    this.form = new FormGroup({
+      title: new FormControl(null, [Validators.required]),
+      body: new FormControl(null, [Validators.required]),
+      done: new FormControl(false),
+      deadline: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  public getIdFromUrl(){
+    this.noteId = this.route.snapshot.params['id'];
+    if(this.noteId){
+      this.getNoteById();
+    }
+  }
+
+  private getNoteById(){
+    this.noteService
+      .getById(this.noteId!)
+      .pipe(first())
+      .subscribe({
+        next: (res) => {
+          this.form.patchValue(res);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      })
+  }
+
+  onSave(){
+    if(this.noteId){
+      this.onUpdate(this.noteId);
+    } else {
+      this.onCreate();
+    }
+  }
+
+  onCreate() {
+    this.submitted = true;
+    if (this.form.valid) {
+      this.noteService
+        .create(this.form.getRawValue())
+        .pipe(first())
+        .subscribe({
+          next: (createdNote: any) => {
+            this.location.back();
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+    }
+  }
+
+  onCancel() {
+    this.submitted = false;
+    this.form.reset();
+  }
+
+  onUpdate(noteId: string){
+    this.submitted = true;
+    if(this.form.valid){
+      this.noteService
+        .update(noteId, this.form.getRawValue())
+        .pipe(first())
+        .subscribe({
+          error: (err) => {
+            console.log(err);
+          },
+          complete: () => {
+            this.location.back()
+          }
+        })
+    }
+  }
 }
