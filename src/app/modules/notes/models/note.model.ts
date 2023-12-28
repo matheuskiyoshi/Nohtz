3f9fd8b5b1db20fb6a491cdb7f84f7c3cc98cf26
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-27 23:32:59 -0300

    feat(note): Create 'note' model

---
+++ src/app/modules/notes/models/note.model.ts
@@ -0,0 +1,10 @@
+export interface Note{
+  _id?: string;
+  title: string;
+  body: string;
+  done: boolean;
+  deadline: string;
+  author: string;
+  created_at: string;
+  updated_at: string;
+}
